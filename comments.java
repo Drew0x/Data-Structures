79a20b73a9bc20cfec389967f0ded736639707c1
Drew <andrew.stephens.professional@gmail.com>
2025-12-18 11:02:13 -0700

    Create comments.java

---
+++ comments.java
@@ -0,0 +1,24 @@
+//Precondition
+/** Computes the sqaure root of a number.
+  @param x A real number >= 0.
+  @return The square root of x.
+*/
+
+//Post condition
+/** Computes the sqaure root of a number.
+  @param x A real number.
+  @return The square root of x if x >= 0.
+  @throws ArithmeticException if x < 0.
+*/
+
+
+//Assert Statement:
+int max = 0;
+for (int index = 0; index < array; index++) {
+  if (array[index] > max)
+    max = array[index];
+  //Assertion:
+  assert sum > 0 : sum;
+} //end for
+
+
